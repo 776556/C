65fe61ad6963be8be1651feade849e13316934c4
jiesen <f23939889@yahoo.com.tw>
2018-09-19 22:10:42 +0800

    便當

--- Bento_system/Form1.Designer.cs
+++ Bento_system/Form1.Designer.cs
@@ -186,7 +186,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(548, 143);
+            this.button2.Location = new System.Drawing.Point(549, 132);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 14;
